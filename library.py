5c44cf2e9b2c7da88288a935f57f9077fbb38930
tcasey <hello@tcasey.me>
2018-06-21 16:28:20 -0600

    🎉 turning in 🎉

--- library.py
+++ library.py
@@ -5,6 +5,18 @@ _mixed_ordinal_pat = _whole_word(r'-?\d+(st|th|nd|rd)')
 _integer_pat = _whole_word(r'\d+')
 _floating_point_after_pat = re.compile(r'\.\d+[^a-zA-Z.]')
 _floating_point_before_pat = re.compile(r'(?<=\d\.)')
+_date_iso8601_pat = _whole_word(r'\d{4}-(0\d|1[0-2])-(0[1-9]|[12][0-9]|3[01])')
+_date_fm2_pat = _whole_word(r'\d{2} (Jan|Feb|Mar|Apr|Jul|Aug|Sep|Oct|Nov|Dec) \d{4}')
+
+def dates_fmt2(text):
+    '''Find date in pattern like 25 Jan 2017'''
+    for match in _date_fm2_pat.finditer(text):
+        yield('ordinal', match)
+
+def dates_iso8601(text):
+    '''Find tokens that begin with a number, and then have an ending like 1st or 2nd.'''
+    for match in _date_iso8601_pat.finditer(text):
+        yield('date', match)
 
 def mixed_ordinals(text):
     '''Find tokens that begin with a number, and then have an ending like 1st or 2nd.'''
--- library_test.py
+++ library_test.py
@@ -7,7 +7,7 @@ began in 1845 (the twenty-second anniversary of the Mexican Revolution), and
 is the 1st example of a national independence holiday becoming popular in the
 Western Hemisphere. (The Fourth of July didn't see regular celebration in the
 US until 15-20 years later.) It is celebrated by 77.9% of the population--
-trending toward 80.                                                                
+trending toward 80.
 '''
 
 class TestCase(unittest.TestCase):
@@ -30,6 +30,59 @@ class TestCase(unittest.TestCase):
     def test_no_integers(self):
         self.assert_extract("no integers", library.integers)
 
+    # Fourth unit test; test date with format 2015-07-25
+    def test_dates(self):
+        self.assert_extract('I was born on 2015-07-25.', library.dates_iso8601, '2015-07-25')
+
+    # Fifth unit test; test date with invalid months/days
+    def test_incorrect_date(self):
+        self.assert_extract("2015-13-48", library.dates_iso8601)
+
+    # Sixth unit test; test date with format 25 Jan 2017
+    def dates_fmt2(self):
+        self.assert_extract('I was born on 25 Jan 2017.', library.dates_fmt2, '25 Jan 2017')
+
+    # Bunch-o-tests
+
+    # test iso8601 timeStamp with format 2018-06-22T18:22:19.123
+    def test_time_stamp(self):
+       self.assert_extract('Current timeStamp is 2018-06-22T18:22:19.123', library.dates_iso8601, '2018-06-22T18:22:19.123')
+
+    # test iso8601 timeStamp with format 2018-06-22T18:22:19.123MDT
+    def test_time_stamp2(self):
+        self.assert_extract('Current timeStamp is 2018-06-22T18:22:19.123MDT', library.dates_iso8601, '2018-06-22T18:22:19.123MDT')
+
+    # test iso8601 timeStamp with format 2018-06-22T18:22:19.123MST
+    def test_time_stamp3(self):
+        self.assert_extract('Current timeStamp is 2018-06-22T18:22:19.123MST', library.dates_iso8601, '2018-06-22T18:22:19.123MST')
+
+    # test  iso8601 timeStamp with format 2018-06-22T18:22:19.123Z
+    def test_time_stamp4(self):
+        self.assert_extract('Current timeStamp is 2018-06-22T18:22:19.123Z', library.dates_iso8601, '2018-06-22T18:22:19.123Z')
+
+    # test iso8601 timeStamp with format 2018-06-22T18:22:19.123-800
+    def test_time_stamp5(self):
+        self.assert_extract('Current timeStamp is 2018-06-22T18:22:19.123-800', library.dates_iso8601, '2018-06-22T18:22:19.123-800')
+
+    # test iso8601 timeStamp with wrong format
+    def test_time_stamp6(self):
+        self.assert_extract('Current timeStamp is 2018-06-22T18:22:19.12', library.dates_iso8601, '2018-06-22T18:22:19.12')
+
+    # test date format 2 with format 25 Jan, 2017
+    def test_date_fmt2_comma(self):
+        self.assert_extract('Current date is 25 Jan, 2017', library.dates_fmt2, '25 Jan, 2017')
+
+    # test date format 2 with format 25 Jan. 2017
+    def test_date_fmt2_comma2(self):
+        self.assert_extract('Current date is 25 Jan. 2017', library.dates_fmt2)
+
+    # test number & letters with comma separated groupings
+    def test_invalid_integers_comma(self):
+        self.assert_extract('I have 123a,456b,789c', library.integers, '123a,456b,789c')
+
+    # test number with comma separated groupings
+    def test_invalid_integers_comma2(self):
+        self.assert_extract('I have 123,456,789', library.integers)
 
 if __name__ == '__main__':
     unittest.main()
